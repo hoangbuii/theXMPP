164f5453b75b0bd53955e75959e8b177a69824ea
Hoàng Bùi <60215764+hoangbuii@users.noreply.github.com>
2023-05-08 20:04:02 +0700

    Chat to each other

--- app1/src/main/java/com/thexmpp/App.java
+++ app1/src/main/java/com/thexmpp/App.java
@@ -1,5 +1,7 @@
 package com.thexmpp;
 
+import java.util.Scanner;
+
 import org.jivesoftware.smack.*;
 import org.jivesoftware.smack.chat2.*;
 import org.jivesoftware.smack.tcp.*;
@@ -21,17 +23,24 @@ public final class App {
      * @throws XmppStringprepException
      */
     public static void main(String[] args) throws XmppStringprepException {
-        System.out.println("Hello World!");
+        //System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
         String server = "127.0.0.1";
         int port = 5222;
-        String username = "hai";
-        String password = "696685";
+        System.out.print("Enter your Username: ");
+        String username = sc.nextLine(); 
+        System.out.print("Password: ");       
+        String password = sc.nextLine();
+        System.out.println("Connectting to server...");
+        // String username = "hoang";        
+        // String password = "hoang";
+        
         
         // Create a connection configuration
         XMPPTCPConnectionConfiguration config = XMPPTCPConnectionConfiguration.builder()
             .setHost(server)
             .setPort(port)
-            .setXmppDomain("test.com")
+            .setXmppDomain("hoangbuii.com")
             .setUsernameAndPassword(username, password)
             .setSecurityMode(ConnectionConfiguration.SecurityMode.disabled)
             .build();
@@ -40,23 +49,39 @@ public final class App {
         AbstractXMPPConnection conn = new XMPPTCPConnection(config);
         
         try {
-          // Connect to the server
-          conn.connect();
-          
-          // Log in to the server
-          conn.login();
-        // get the chatManager 
-        ChatManager chatManager = ChatManager.getInstanceFor(conn);
-        
-        // Create a Chat object with the recipient's JID
-        EntityBareJid jid = JidCreate.entityBareFrom("hai@test.com");
-        Chat chat = chatManager.chatWith(jid);
-        
-        // Send a message to the recipient
-        String messageText = "Hello, how are you?";
-        Message message = new Message();
-        message.setBody(messageText);
-        chat.send(message);
+            // Connect to the server
+            conn.connect();
+            
+            // Log in to the server
+            conn.login();
+            // get the chatManager 
+            System.out.println("Connect succserfully!");
+            ChatManager chatManager = ChatManager.getInstanceFor(conn);
+            
+            System.out.println("Enter user to start a chat: ");
+            String useString = sc.nextLine();
+            // Create a Chat object with the recipient's JID
+            EntityBareJid jid = JidCreate.entityBareFrom(useString + "@hoangbuii.com");
+            Chat chat = chatManager.chatWith(jid);
+            
+            //Send a message to the recipient
+            //String messageText = "Hello, how are you?";
+            //chat.send(messageText);
+
+            chatManager.addIncomingListener(new IncomingChatMessageListener() {
+              @Override
+              public void newIncomingMessage(EntityBareJid from, Message message, Chat chat) {
+                  System.out.println(from + ": " + message.getBody());
+              }
+            });
+
+            while (true) {
+              String msg = sc.nextLine();
+              chat.send(msg);
+              // if ("bye".equalsIgnoreCase(msg)) {
+              //   break;
+              // }
+            }
           
         } catch (Exception e) {
           e.printStackTrace();
