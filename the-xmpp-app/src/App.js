d6e4ca099c12d9aa8c15d37c325d99f617bd745b
Nguyen Thanh An - 20021280 <87203086+mrCoding85@users.noreply.github.com>
2023-05-20 00:22:22 +0700

    Update App.js

--- the-xmpp-app/src/App.js
+++ the-xmpp-app/src/App.js
@@ -1,11 +1,19 @@
 import About from "./pages/about/about";
+
 import Home from "./pages/home/home";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Nopage from "./pages/nopage/nopage";
+
 import Layout from "./pages/layout";
+
 import Tempdetail from "./pages/detail/tempdetail";
+
 import Humiddetail from "./pages/detail/humiddetail";
+
 import Atmdetail from "./pages/detail/atmdetail";
+
 import Nodeinfodetail from "./pages/nodeinfodetail/nodeinfodetail";
 
 function App() {
@@ -24,6 +32,7 @@ function App() {
       </Routes>
     </BrowserRouter>
   )
+  
 }
 
 export default App;
