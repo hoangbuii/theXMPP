29da17ced3d31817b3794b36ecb2defa140c0535
Nguyen Thanh An - 20021280 <87203086+mrCoding85@users.noreply.github.com>
2023-05-20 00:53:46 +0700

    Update atm.js

--- the-xmpp-app/src/components/inhome/atm/atm.js
+++ the-xmpp-app/src/components/inhome/atm/atm.js
@@ -6,12 +6,19 @@ import { Link } from 'react-router-dom'
 export default function AtmHome(props) {
   return (
     <div className='atm-home-container'>
+    
         <div className='title-container'>
+    
             <div className='img-atm-container'>
+    
                 <img src='../../assets/atm.png' alt='no-img' className='img'/>
+    
             </div>
+    
             <div className='name-atm'>Pressure</div>
+    
         </div>
+    
         <div className='content-atm-container'>
             <div className='avg-atm'>{ props.avg }</div>
             <div className='atm-unit'>mbar</div>
