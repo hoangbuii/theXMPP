b7642236b782b92031e8208454f2ccc915a4d3bc
Do Huy Anh <dohuyanh999@gmail.com>
2023-05-19 23:39:17 +0700

    Update about.js

--- the-xmpp-app/src/pages/about/about.js
+++ the-xmpp-app/src/pages/about/about.js
@@ -8,14 +8,13 @@ export default function About() {
     <div>
         <Header />
           <div className='about-container'>
-          <div className='logo-container-large'>
-            <img src='./assets/logo.png' alt='no-img' className='img' />
-          </div> 
-          <div className='description-container'> 
-            The implementation of XMPP (Extensible Messaging and Presence Protocol) for sensor networks has become increasingly popular in recent years. XMPP is a widely-used communication protocol that allows for the exchange of data between devices, making it an ideal choice for use in sensor networks. This report provides an overview of the implementation of XMPP for sensor networks using Openfire server and Smack library.<br/>
-            The implementation section provides a detailed guide for setting up and configuring the Openfire server and Smack library for use in a sensor network. This section includes information about configuring the server, installing and configuring the Smack library, and setting up communication between devices.<br/>
-            Overall, this report provides a comprehensive overview of the implementation of XMPP for sensor networks using Openfire server and Smack library. It is intended to be a useful resource for those interested in developing and implementing XMPP-based solutions for sensor networks, as well as for those who want to better understand the benefits and challenges associated with this approach.<br/>
-          </div>    
+            <div className='logo-container-large'>
+              <img src='./assets/logo.png' alt='no-img' className='img'/>
+            </div> 
+            <div className='description-container'> 
+              The implementation of XMPP (Extensible Messaging and Presence Protocol) for sensor networks has become increasingly popular in recent years. XMPP is a widely-used communication protocol that allows for the exchange of data between devices, making it an ideal choice for use in sensor networks. This report provides an overview of the implementation of XMPP for sensor networks using Openfire server and Smack library.<br/>
+              The implementation section provides a detailed guide for setting up and configuring the Openfire server and Smack library for use in a sensor network. This section includes information about configuring the server, installing and configuring the Smack library, and setting up communication between devices.<br/>
+            </div>    
           </div>
         <Footer />
     </div>
