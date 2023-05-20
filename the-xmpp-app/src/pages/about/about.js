773b5c34e4b96563f4c9ee7e605765aa03f81cc2
Do Huy Anh <63349028+huyanh1309@users.noreply.github.com>
2023-05-20 13:01:09 +0700

    Update about.js

--- the-xmpp-app/src/pages/about/about.js
+++ the-xmpp-app/src/pages/about/about.js
@@ -11,8 +11,10 @@ export default function About() {
             <div className='logo-container-large'>
               <img src='./assets/logo.png' alt='no-img' className='img'/>
             </div> 
-            <div className='description-container'> 
-              
+              <div className='description-container'> 
+                TheXMPP project is built based on the XMPP Protocol which leverages the Openfire platform to setup and put to work. The platform uses various technology such as NodeJS, ReactJS, etc... to demonstrate how we can use sensors to communicate with the servers. <br>
+                The program uses sensors to detect and calculate the temperature, humidity, and atmospheric pressure in the air and send those information to a server in the form of a packet, which then can be sent back to users on the other end. <br>
+              </div>
             </div>    
           </div>
         <Footer />
